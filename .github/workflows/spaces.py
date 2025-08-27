c425423432546342a83ca4a8602f3fb30dde90b6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-08-27 03:04:43 +0100

    Update .github/workflows/spaces.py

--- .github/workflows/spaces.py
+++ .github/workflows/spaces.py
@@ -21,7 +21,7 @@ def main(repo_id: str, token: str, repo_type: str, folder_path: str):
 
     destination = huggingface_hub.upload_folder(
         repo_id=repo_id, folder_path=folder_path, commit_message='Updating', token=token, repo_type=repo_type,
-        ignore_patterns=["*.safetensors"]
+        ignore_patterns=["*.safetensors", ".gitattributes"]
     )
     logging.info(destination)
 
