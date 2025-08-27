4b07c8a65cff93a9c3723cd64bd1d46d9b9fd15e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-08-27 02:28:55 +0100

    Add .github/workflows/spaces.py

---
+++ .github/workflows/spaces.py
@@ -0,0 +1,31 @@
+"""Module spaces.py"""
+import logging
+
+import fire
+import huggingface_hub
+
+
+def main(repo_id: str, token: str, repo_type: str, folder_path: str):
+    """
+
+    :param repo_id:
+    :param token:
+    :param repo_type:
+    :param folder_path:
+    :return:
+    """
+
+    url = huggingface_hub.create_repo(
+        repo_id=repo_id, token=token, private=False, repo_type=repo_type, exist_ok=True, space_sdk='gradio')
+    logging.info(url)
+
+    destination = huggingface_hub.upload_folder(
+        repo_id=repo_id, folder_path=folder_path, commit_message='Updating', token=token, repo_type=repo_type,
+        ignore_patterns=["*.safetensors"]
+    )
+    logging.info(destination)
+
+
+if __name__ == '__main__':
+
+    fire.Fire(main)
